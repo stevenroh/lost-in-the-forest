a052dfe902922b5dcc4c929835df85a96639d374
Yvan Pannatier <yvan.pannatier@hevs.ch>
2022-05-08 16:56:50 +0200

    feat: Add stackable items

--- src/characters/Player/inventory/Inventory.gd
+++ src/characters/Player/inventory/Inventory.gd
@@ -18,11 +18,22 @@ func slot_gui_input(event: InputEvent, slot: SlotClass):
 					slot.put_in_slot(holding_item)
 					holding_item = null
 				else:
-					var temp_item = slot.item
-					slot.pick_from_slot()
-					temp_item.global_position = event.global_position
-					slot.put_in_slot(holding_item)
-					holding_item = temp_item
+					if holding_item.item_name != slot.item.item_name:
+						var temp_item = slot.item
+						slot.pick_from_slot()
+						temp_item.global_position = event.global_position
+						slot.put_in_slot(holding_item)
+						holding_item = temp_item
+					else:
+						var stack_size = int(ItemDataReader.item_data[slot.item.item_name]['stackSize'])
+						var can_be_added = stack_size - slot.item.item_quantity
+						if can_be_added > holding_item.item_quantity:
+							slot.item.add_item_quantity(holding_item.item_quantity)
+							holding_item.queue_free()
+							holding_item = null
+						else:
+							slot.item.add_item_quantity(can_be_added)
+							holding_item.decrease_item_quantity(can_be_added)
 			elif slot.item:
 				holding_item = slot.item
 				slot.pick_from_slot()
--- src/characters/Player/inventory/Item.gd
+++ src/characters/Player/inventory/Item.gd
@@ -1,9 +1,34 @@
 extends Node2D
 
+var item_name
+var item_quantity
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if randi() % 2 == 0:
-		$TextureRect.texture = load("res://assets/Items/cursorSword_bronze.png")
+	var rand_val = randi() % 4
+	if rand_val == 0:
+		item_name = 'axe'
+	elif rand_val == 1:
+		item_name = 'pickaxe'
+	elif rand_val == 2:
+		item_name = 'wood'
 	else:
-		$TextureRect.texture = load("res://assets/Items/cursorSword_gold.png")
+		item_name = 'woodstick'
+	
+	$TextureRect.texture = load("res://assets/Items/"+ item_name +".png")
+	var stack_size = int(ItemDataReader.item_data[item_name]["stackSize"])
+	item_quantity = randi() % stack_size + 1
+	
+	if stack_size == 1:
+		$Label.visible = false
+	else:
+		$Label.text = String(item_quantity)
+		
+func add_item_quantity(amount_to_add):
+	item_quantity += amount_to_add
+	$Label.text = String(item_quantity)
+
+func decrease_item_quantity(amount_to_remove):
+	item_quantity -= amount_to_remove
+	$Label.text = String(item_quantity)
